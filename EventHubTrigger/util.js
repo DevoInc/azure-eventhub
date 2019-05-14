7b3c6b0b4a81ea2938be4a0f0e3bd27951d556e1
joseramon.afonso <joseramon.afonso@devo.com>
2019-05-14 11:41:49 +0200

    few little changes of linter

--- EventHubTrigger/util.js
+++ EventHubTrigger/util.js
@@ -1,22 +1,21 @@
-const devo = require('@devo/nodejs-sdk');
-const dateFormat = require('dateformat');
-const os = require("os");
-const config = require('./config.json');
+const devo = require("@devo/nodejs-sdk");
+const dateFormat = require("dateformat");
+const config = require("./config.json");
 
 module.exports = {
-    devoLogs: function (devo_sender) {
+    devoLogs: function (devoSender) {
         let senders = {};
         let tables = {
-            'stats': `devo.agent.stats`,
-            'out': `devo.agent.out`
+            "stats": `devo.agent.stats`,
+            "out": `devo.agent.out`
         };
 
         /**
          * Initialize the senders
          */
         for (let opt in tables) {
-            let conf = Object.assign({}, devo_sender);
-            conf['tag'] = tables[opt];
+            let conf = Object.assign({}, devoSender);
+            conf["tag"] = tables[opt];
             senders[opt] = devo.sender(conf);
         };
 
@@ -26,13 +25,13 @@ module.exports = {
         this.sendLog = function(log) {
             if (config.send_logs === false) return;
             message = {
-                'time': dateFormat(new Date(), 'isoUtcDateTime'),
-                'agent': 'azure-eventhub-function',
-                'level': 'INFO',
-                'name': 'devo-function-app',
-                'info': log
+                "time": dateFormat(new Date(), "isoUtcDateTime"),
+                "agent": "azure-eventhub-function",
+                "level": "INFO",
+                "name": "devo-function-app",
+                "info": log
             }
-            senders['out'].send(JSON.stringify(message));
+            senders["out"].send(JSON.stringify(message));
         };
 
         /**
@@ -41,12 +40,12 @@ module.exports = {
         this.sendStats = function(events, events_size) {
             if (config.send_stats === false) return;
             message = {
-                'time': dateFormat(new Date(), 'isoUtcDateTime'),
-                'agent': 'azure-eventhub-function',
-                'name': 'devo-function-app',
-                'info': `${events} ${events_size}`
+                "time": dateFormat(new Date(), "isoUtcDateTime"),
+                "agent": "azure-eventhub-function",
+                "name": "devo-function-app",
+                "info": `${events} ${events_size}`
             };
-            senders['stats'].send(JSON.stringify(message));
+            senders["stats"].send(JSON.stringify(message));
         }
 
         return this;
@@ -54,17 +53,17 @@ module.exports = {
 
     formatRegion: function(region) {
         let slugify = function(string) {
-            const a = 'àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;';
-            const b = 'aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------';
-            const p = new RegExp(a.split('').join('|'), 'g');
+            const a = "àáäâãåăæçèéëêǵḧìíïîḿńǹñòóöôœøṕŕßśșțùúüûǘẃẍÿź·/_,:;";
+            const b = "aaaaaaaaceeeeghiiiimnnnooooooprssstuuuuuwxyz------";
+            const p = new RegExp(a.split("").join("|"), "g");
             return string.toString().toLowerCase()
-                .replace(/\s+/g, '-')
+                .replace(/\s+/g, "-")
                 .replace(p, c => b.charAt(a.indexOf(c)))
-                .replace(/&/g, '-and-')
-                .replace(/[^\w\-]+/g, '')
-                .replace(/\-\-+/g, '-')
-                .replace(/^-+/, '')
-                .replace(/-+$/, '')
+                .replace(/&/g, "-and-")
+                .replace(/[^\w\-]+/g, "")
+                .replace(/\-\-+/g, "-")
+                .replace(/^-+/, "")
+                .replace(/-+$/, "")
         };
         return slugify(region);
     }
